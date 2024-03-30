b4a3f0333456447c67a269785fa9493118821aff
Teresiawangui <wanguiiteresiaa@gmail.com>
2024-03-31 00:28:55 +0300

    tuple

---
+++ tuple.py
@@ -0,0 +1,13 @@
+# Add
+fruits =("apple","mango","kiwi")
+newfruits = fruits + ("dates",)
+print(newfruits)
+
+# remove
+vegetables =("kales","tomato","carrots") 
+nwevegetables = list(vegetables)
+nwevegetables.remove("carrots")
+vegetables = tuple(nwevegetables)
+print(vegetables)
+
+
